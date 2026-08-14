bd8129642eca97d7839a86458f71474bd226af84
manojnambiraj <postboxmano@gmail.com>
2026-08-14 20:33:49 +0530

    Java Basics upto arrays

---
+++ MyArray.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class MyArray {
+    static void main(String[] args) {
+//        int[] arr = {10, 20, 30, 40, 50};
+//                    0   1   2.........
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+
+//        for (int item : arr) {
+//            System.out.println(item);
+//        }
+
+//        int[] arr = new int[5];
+//
+//        arr[0] = 1;
+//        arr[1] = 20;
+//        arr[2] = 77;
+//        arr[3] = 9;
+//        arr[4] = 78;
+
+//        for(int i: arr) {
+//            System.out.println(i);
+//        }
+
+//        String[][] fruits = {{"a", "b"}, {"c", "d"}};
+
+//        for(String[] i : fruits){
+//            for(String j : i){
+//                System.out.println(j);
+//            }
+//        }
+
+
+
+
+    }
+}
---
+++ MyVar.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class MyVar {
+    static void main(String[] args) {
+//     Operators:
+//        Arithmetic operators --> (+, -, *, /, %, **, ++, --)
+//        Assignment operators --> (=, +=, -=, *=, /=, %=, **=)
+//        Comparison operators --> (==, !=, >, >=, <, <=) --> Boolean
+//        Logical operators    --> (&&, ||, !)
+//
+//       Type Casting
+//        float a = 5.66f;
+//        int b = (int) a;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        Strings:
+
+//        String text = "Hello";
+
+//        System.out.println(text.concat("World!"));
+//        System.out.println(text.charAt(2));
+//        System.out.println(text + " " +"World!");
+//        System.out.println(text.toUpperCase());
+//        System.out.println(text.toLowerCase());
+//        System.out.println(text.equals("Hellos"));
+//        System.out.println(text == "Hello");
+
+//        String s = "If you add two 'strings',the result will be a string concatenation:";
+//
+//        System.out.println(s);
+
+//        Math:
+//        int a = (int)(Math.random() * 999999);
+//        int a = Math.max(2, 5);
+//        int a = Math.min(2, 5);
+//        System.out.println(a);
+
+//        Control Stmts:
+//            1. Conditional stmts
+//                if
+//                if else
+//                if else if
+//                switch case
+
+//            2. Looping stmts
+//                for
+//                while
+//                do..while
+
+//            3. Jumping stmts
+//                 break
+//                 continue
+
+
+    }
+}
